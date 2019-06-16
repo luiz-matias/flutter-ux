9e0d6f10d0878229b8d20ac5f8e469af6174778b
Luiz Matias <luiz.matias1999@gmail.com>
2019-06-16 15:26:29 -0300

    Terminado módulo sobre widgets do curso

--- flutter_first_app/flutter_first_app/lib/home_page.dart
+++ flutter_first_app/flutter_first_app/lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'common_widgets/drawer.dart';
 import 'pages/chat_room_page.dart';
 import 'pages/list_page.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,68 +10,114 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _scaffoldContext;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      primary: true,
-      appBar: AppBar(
-        title: Text("Truck Help"),
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: Container(
-          color: Colors.black87,
-          child: ListView(
-            children: <Widget>[
-              _drawerItem("http://chittagongit.com/download/23827", "Conversas",
-                  () => navigateToPage(ChatRoomPage())),
-              _drawerItem("http://chittagongit.com/download/23827", "Lista",
-                  () => navigateToPage(ListPage())),
-              _drawerItem("http://chittagongit.com/download/23827", "Conversas",
-                  () => navigateToPage(ChatRoomPage())),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        primary: true,
+        appBar: AppBar(
+          title: Text("Truck Help"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(text: "Tab 1", icon: Icon(Icons.favorite),),
+              Tab(text: "Tab 2", icon: Icon(Icons.favorite),),
+              Tab(text: "Tab 3", icon: Icon(Icons.favorite),),
             ],
           ),
         ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.black87,
+            child: ListView(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("Luiz Matias"),
+                  accountEmail: Text("luiz.matias@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://banner2.kisspng.com/20180329/bpq/kisspng-avatar-education-professor-user-profile-faculty-boss-5abcab3d64aff2.9884136415223140454124.jpg"),
+                  ),
+                ),
+                _drawerItem(Icons.people, "Conversas",
+                    () => navigateToPage(ChatRoomPage())),
+                _drawerItem(
+                    Icons.list, "Lista", () => navigateToPage(ListPage())),
+                _drawerItem(Icons.people, "Conversas",
+                    () => navigateToPage(ChatRoomPage())),
+              ],
+            ),
+          ),
+        ),
+        body: TabBarView(
+            children: <Widget> [
+              Builder(builder: (context) {
+                _scaffoldContext = context;
+                return SafeArea(child: _body());
+              }),
+              Builder(builder: (context) {
+                _scaffoldContext = context;
+                return SafeArea(child: _body());
+              }),
+              Builder(builder: (context) {
+                _scaffoldContext = context;
+                return SafeArea(child: _body());
+              }),
+            ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _onClickToast();
+          },
+          child: Icon(Icons.add),
+        ),
       ),
-      body: SafeArea(child: _body()),
     );
   }
 
   _body() {
     return Center(
-      child: Container(
-        color: Colors.white,
-        child: Text("Olá mundo!"),
+      child: SizedBox.expand(
+        child: Container(
+            color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () => _onClickSnackbar(),
+                    child: Text("Snackbar"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _onClickAlertDialog(),
+                    child: Text("AlertDialog"),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _onClickToast(),
+                    child: Text("Toast"),
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
 
-  _drawerItem(String image, String title, Function onTap) {
-    return InkWell(
+  _drawerItem(IconData icon, String title, Function onTap) {
+    return ListTile(
       onTap: onTap,
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Row(
-          children: <Widget>[
-            Image.network(
-              image,
-              width: 12,
-              height: 12,
-              color: Colors.white,
-              fit: BoxFit.contain,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 32.0),
-              child: Text(
-                title,
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-            )
-          ],
-        ),
+      leading: Icon(
+        icon,
+        color: Colors.white,
       ),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      ),
+      subtitle: Text(title, style: TextStyle(color: Colors.grey)),
     );
   }
 
@@ -79,5 +125,46 @@ class _HomePageState extends State<HomePage> {
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return page;
     }));
+  }
+
+  _onClickSnackbar() {
+    Scaffold.of(_scaffoldContext).showSnackBar(SnackBar(
+      content: Text("Olá mundo!"),
+    ));
+  }
+
+  _onClickAlertDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Título"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancelar"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("Entendi"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  _onClickToast() {
+    Fluttertoast.showToast(
+        msg: "Olá mundo!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.black54,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 }
